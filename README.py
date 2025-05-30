bc28aead00b10de03ebc1174c6c9f6ce2a369434
bianunexz <bianunexz@gmail.com>
2025-05-30 09:46:41 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,28 +1,46 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
-   import pandas as pd
-   import matplotlib.pyplot as plt
+# Título do aplicativo
+st.title("Análise de Dados do Buteco")
 
-   # Título do aplicativo
-   st.title("Análise de Dados do Buteco")
+# Carregar o arquivo CSV
+file = st.file_uploader("Escolha um arquivo CSV", type='csv')
 
-   # Carregar o arquivo CSV
-   file = st.file_uploader("Escolha um arquivo CSV", type='csv')
+if file is not None:
+    # Ler o arquivo CSV
+    data = pd.read_csv(file)
 
-   if file is not None:
-       # Ler o arquivo CSV
-       data = pd.read_csv(file)
+    # Mostrar os dados
+    st.write("Dados Carregados:")
+    st.dataframe(data)
 
-       # Mostrar os dados
-       st.write("Dados Carregados:")
-       st.dataframe(data)
+    # Gráfico de Análise: Quantidade de botecos por bairro
+    st.subheader("Quantidade de Botecos por Bairro")
+    
+    # Contar a quantidade de botecos por bairro
+    bairro_counts = data['bairro'].value_counts()
 
-       # Criar um gráfico
-       st.subheader("Gráfico de Análise")
-       plt.figure(figsize=(10, 5))
-       plt.bar(data['nome'], data['prato'])  # Exemplo de gráfico, ajuste conforme necessário
-       plt.xlabel('Nome do Buteco')
-       plt.ylabel('Prato')
-       plt.title('Pratos dos Butecos')
-       plt.xticks(rotation=45)
-       st.pyplot(plt)
-   
+    # Criar o gráfico
+    plt.figure(figsize=(10, 5))
+    bairro_counts.plot(kind='bar', color='skyblue')
+    plt.xlabel('Bairro')
+    plt.ylabel('Quantidade de Botecos')
+    plt.title('Quantidade de Botecos por Bairro')
+    plt.xticks(rotation=45)
+    st.pyplot(plt)
+
+    # Gráfico de Análise: Pratos mais populares
+    st.subheader("Pratos Mais Populares")
+    
+    # Contar a quantidade de pratos
+    prato_counts = data['prato'].value_counts()
+
+    # Criar o gráfico
+    plt.figure(figsize=(10, 5))
+    prato_counts.plot(kind='bar', color='lightgreen')
+    plt.xlabel('Prato')
+    plt.ylabel('Quantidade')
+    plt.title('Pratos Mais Populares')
+    plt.xticks(rotation=45)
+    st.pyplot(plt)
